13e479bdf410649d29f31de97d903bb12ac1a8b0
Fabricio Alegre <fabricioalegre9828@gmail.com>
2026-06-04 00:18:48 -0300

    feat: add institutional Material Design 3 color palette in Colors.ts

---
+++ constants/Colors.ts
@@ -0,0 +1,13 @@
+/**
+ * Material Design 3 Institutional Colors for FollowUp
+ */
+export const Colors = {
+  primary: '#6750A4',     // Morado principal
+  secondary: '#625B71',   // Textos secundarios
+  neutral: '#1C1B1F',     // Textos fuertes
+  background: '#FDFBFF',  // Fondo base
+  surface: '#F4F3F7',     // Fondo de tarjetas
+  border: '#CAC4D0',      // Bordes
+};
+
+export default Colors;
